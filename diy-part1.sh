91671f8418d0a1aeb9b464c7201f98ab9ff9721a
rsd1987 <rsd1987@qq.com>
2021-12-31 21:24:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,6 +11,7 @@
 #
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Other customizations
 mkdir package/custom
